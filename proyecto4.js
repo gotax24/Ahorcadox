b340af1b0f7ceef145a200afc7fad8711956795d
gotax24 <124403304+gotax24@users.noreply.github.com>
2024-07-31 19:06:29 -0500

    Agregar documentacion

--- proyecto4.js
+++ proyecto4.js
@@ -1,3 +1,4 @@
+//variables globales para todas las funciones 
 let rayas = [];
 let texto = [];
 let fallidos = [];
@@ -5,11 +6,15 @@ let p = document.getElementById("result");
 let intentos = 10;
 let resultado = document.getElementById("resultado");
 
+
+//funcion para resetear los input
 const reset = () => {
   document.getElementById("palabra").value = "";
   document.getElementById("intentos").value = "";
 };
 
+
+//funcion para reiniciar el juego despues de 15 sg de ganar o perder
 const reiniciar = () => {
   rayas = [];
   fallidos = [];
@@ -20,15 +25,19 @@ const reiniciar = () => {
   document.getElementById("fallidos").innerHTML = "";
 };
 
+
+//funcion para iniciar el juego solo se una vez por partida
 const iniciar = () => {
   let palabra = document.getElementById("palabra").value;
 
+  //si la palabra esta vacia no se inicia
   if (palabra === "") {
     alert("La palabra esta vacia");
   } else {
     texto = palabra.split("");
-    resultado.innerHTML = `Te quedan ${10} para ganar`;
+    resultado.innerHTML = `Te quedan ${10} intentos`;
 
+    //verificacioon para que no sigan agregando palabras
     if (rayas.length > 0) {
       alert("Tienes que adivinar primero la palabra para agregar otra");
     } else {
@@ -40,11 +49,13 @@ const iniciar = () => {
   }
 };
 
+//funcion para comprobar letra por letra en el juego
 const comprobrar = () => {
   let letra = document.getElementById("intentos").value;
   let ol = document.getElementById("fallidos");
   let enconntrada = false;
 
+  //forEach para encontrar las letras y cambiar el status de encontradas
   texto.forEach((element, index) => {
     if (element === letra) {
       rayas[index] = letra;
@@ -53,8 +64,10 @@ const comprobrar = () => {
     }
   });
 
+
+  //Si encontrada es falso se agrega la letra a las fallidas que se muestra en el html
   if (!enconntrada) {
-    if (!fallidos.includes(letra)) {
+    if (!fallidos.includes(letra)) {//si la letra no esta agregada se agrega para que no se repitan las letras
       fallidos.push(letra);
       ol.innerHTML = `Letras que no están: ${fallidos.join(", ")}`;
       intentos--; // Decrementa el número de intentos al fallar
@@ -65,24 +78,29 @@ const comprobrar = () => {
   // Actualiza el mensaje de intentos restantes
   resultado.innerHTML = `Te quedan ${intentos} intentos`;
 
+  //si el numero de intentos llega a 0 se ejecuta la funcion loser
   if (intentos === 0) {
     loser();
-  } else if (rayas.every((element, index) => element === texto[index])) {
+  } else if (rayas.every((element, index) => element === texto[index])) { //si se adivina la palabra y ya no hay rayas se ejecuta la funcino winner
     winner();
   }
 
+  // Actualiza la palabra mientra que vayan adivinando 
   p.innerHTML = `La palabara es: ${rayas}`;
 };
 
+
+//funcion para cuando el jugador pierde
 const loser = () => {
-  resultado.style.color = "red";
+  resultado.style.color = "red"; //se le cambia el color a rojo al texto
   resultado.innerHTML = `PERDISTE </br> la palabra era: ${texto}. Que loser jajajaja`;
-  setTimeout(reiniciar, 15000);
+  setTimeout(reiniciar, 15000);// se le reinicia el juego despues de 15 sg de perder
 };
 
+//funcion para cuando el jugador gana
 const winner = () => {
-  resultado.style.color = "greenyellow";
+  resultado.style.color = "greenyellow"; //se le cambia el color a verder al texto
   resultado.innerHTML = `GANASTE </br> Adivinaste la palabra era: ${texto}. Enhorabuena matador`;
 
-  setTimeout(reiniciar, 15000);
+  setTimeout(reiniciar, 15000);// se le reinicia el juego despues de 15 sg de ganar
 };
